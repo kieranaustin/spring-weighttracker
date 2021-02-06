c066c7c40ffab517a5918425eecf8d63efebb389
kieranaustin <kieranaustin@gmail.com>
2021-02-06 11:40:21 +0100

    first commit - basic restful hello world example

---
+++ src/main/java/com/weighttracker/WeightTrackerApp.java
@@ -0,0 +1,15 @@
+package com.weighttracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeightTrackerApp
+{
+    
+    public static void main(String[] args)
+    {
+        SpringApplication.run(WeightTrackerApp.class, args);
+    }
+}
+
---
+++ src/test/java/com/weighttracker/WeightTrackerApp.java
@@ -0,0 +1,13 @@
+package com.weighttracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeightTrackerAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
